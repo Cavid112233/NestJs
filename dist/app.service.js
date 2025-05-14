f575bc2f0b0087d9d5c0ba2c59c91c3dbe5563f6
Cavid112233 <cavidsly@gmail.com.com>
2025-05-14 12:28:12 -0700

    SwaggerAdded

--- dist/app.service.js
+++ dist/app.service.js
@@ -10,7 +10,7 @@ exports.AppService = void 0;
 const common_1 = require("@nestjs/common");
 let AppService = class AppService {
     getHello() {
-        return 'heyvere Emin';
+        return 'Heyvere Emin';
     }
 };
 exports.AppService = AppService;
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'heyvere Emin';
+    return 'Heyvere Emin';
   }
 }
